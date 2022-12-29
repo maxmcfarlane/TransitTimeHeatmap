dd122732fcec59aecaf063a4bddb5c384f8e2dec
Max McFarlane <max.mcfarlane@tneigroup.com>
2022-12-29 21:03:41 +0000

    flask app

--- wsgi.py
+++ wsgi.py
@@ -2,11 +2,12 @@
 import flask
 from dashapp import create_app
 
-server = flask.Flask(__name__)
+server = flask.Flask(__name__,
+                     template_folder='templates',
+                     static_folder='static',
+                     instance_relative_config=True)
 
 app = create_app(server)
 
-
 if __name__ == "__main__":
     app.run(debug=False)
-
